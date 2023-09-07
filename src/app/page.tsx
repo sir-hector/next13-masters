e364ebab46ac1b5b196b223e6024c6f8cbfa3529
sir-hector <krauskarol7@gmail.com>
2023-09-07 19:47:25 +0200

    add for test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,9 +69,8 @@ export default function Home() {
 							-&gt;
 						</span>
 					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Learn about Next.js in an interactive course with&nbsp;quizzes!
-					</p>
+					
+					
 				</a>
 
 				<a

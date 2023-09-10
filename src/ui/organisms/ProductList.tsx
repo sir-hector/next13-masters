d0bbd2999d3e381b0a9bf32edb6c26edf0ed4394
sir-hector <krauskarol7@gmail.com>
2023-09-10 16:43:02 +0200

    add id to list

--- src/ui/organisms/ProductList.tsx
+++ src/ui/organisms/ProductList.tsx
@@ -3,7 +3,7 @@ import { type ProductItemType } from "../types";
 
 export const ProductList = ({ products }: { products: ProductItemType[] }) => {
 	return (
-		<ul className="flex justify-center gap-5">
+		<ul className="flex justify-center gap-5" data-testid="products-list">
 			{products.map((product) => (
 				<li key={product.id}>
 					<ProductListItem product={product} />

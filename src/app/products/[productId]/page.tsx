221abe4ddb68a0fd66164ac584cd3b3c081ce757
sir-hector <krauskarol7@gmail.com>
2023-09-13 19:26:35 +0200

    generateStaticParams

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,7 +1,12 @@
 import { Suspense } from "react";
-import { getProductById } from "@/app/api/products";
+import { getProductById, getProductsList } from "@/app/api/products";
 import { ProductListItem } from "@/ui/molecules/ProductListItem";
 import { SuggestedProducts } from "@/ui/organisms/SuggestedProducts";
+
+export const generateStaticParams = async () => {
+	const products = await getProductsList();
+	return products.map((product) => product.id);
+};
 
 export default async function SigleProduct({ params }: { params: { productId: string } }) {
 	const product = await getProductById(params.productId);

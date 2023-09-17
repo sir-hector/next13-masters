59b7f9e2b2287d911215434a99de979e8f248c10
sir-hector <krauskarol7@gmail.com>
2023-09-17 13:24:39 +0200

    sugessted product fix

--- src/ui/organisms/SuggestedProducts.tsx
+++ src/ui/organisms/SuggestedProducts.tsx
@@ -1,10 +1,11 @@
 import { getProductsList } from "@/app/api/products";
 import { ProductList } from "@/ui/organisms/ProductList";
 
-const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+const wait = (ms: number) =>
+	new Promise((resolve) => setTimeout(resolve, ms));
 
 export const SuggestedProducts = async () => {
-	const products = await getProductsList();
+	const products = await getProductsList(5, 0);
 	await wait(5000);
 
 	return <ProductList products={products.slice(-4)} />;

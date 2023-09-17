7f993c99ae40171144f6900c58b517ba1d884b2b
sir-hector <krauskarol7@gmail.com>
2023-09-17 16:48:19 +0200

    connect hygraph

--- src/app/api/products.ts
+++ src/app/api/products.ts
@@ -14,24 +14,90 @@ type ProductResponseItem = {
 	longDescription: string;
 };
 
-export const getProductsList = async (productAmount: number, offset: number) => {
-	const res = await fetch(`https://naszsklep-api.vercel.app/api/products?take=${productAmount}&offset=${offset}`);
-	const productsResponse = (await res.json()) as ProductResponseItem[];
-	const products = productsResponse.map(productResposeItemToProductItemType);
+export const getProductsList = async (
+	productAmount: number,
+	offset: number,
+): Promise<ProductItemType[]> => {
+	const res = await fetch(
+		"https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clmnj7rrp254t01rtdn8v9ex3/master",
+		{
+			method: "POST",
+			body: JSON.stringify({
+				query: /* GraphQL */ `
+					query GetProductsList {
+						products(first: ${productAmount}, skip: ${offset}) {
+							id
+							name
+							description
+							price
+							images {
+								url
+							}
+						}
+					}
+				`,
+			}),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		},
+	);
 
-	return products;
+	type GraphQLResponse<T> =
+		| { data?: undefined; errors: { message: string }[] }
+		| { data: T; errors?: undefined };
+
+	type TResult = {
+		products: {
+			id: string;
+			name: string;
+			description: string;
+			images: {
+				url: string;
+			}[];
+			price: number;
+		};
+	};
+
+	const graphqlResponse =
+		(await res.json()) as GraphQLResponse<TResult>;
+
+	if (graphqlResponse.errors) {
+		throw new Error(graphqlResponse.errors[0].message);
+	}
+
+	// return graphqlResponse.data.products;
+
+	return graphqlResponse.data.products.map((p) => {
+		return {
+			id: p.id,
+			name: p.name,
+			description: p.description,
+			category: p.category,
+			price: p.price,
+			coverImage: {
+				src: p.images[0].url,
+				alt: p.name,
+			},
+		};
+	});
 };
 
 export const getProductById = async (id: string) => {
-	const res = await fetch(`https://naszsklep-api.vercel.app/api/products/${id}`);
+	const res = await fetch(
+		`https://naszsklep-api.vercel.app/api/products/${id}`,
+	);
 	const productResponse = (await res.json()) as ProductResponseItem;
 
-	const product = productResposeItemToProductItemType(productResponse);
+	const product =
+		productResposeItemToProductItemType(productResponse);
 
 	return product;
 };
 
-const productResposeItemToProductItemType = (product: ProductResponseItem): ProductItemType => ({
+const productResposeItemToProductItemType = (
+	product: ProductResponseItem,
+): ProductItemType => ({
 	id: product.id,
 	price: product.price,
 	name: product.title,
